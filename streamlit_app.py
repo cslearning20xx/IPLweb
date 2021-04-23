2b5690f2888c87c97c1695d675a0e2b1000c5646
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2021-04-23 10:52:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from google.cloud import firestore
 import pandas as pd
 import pytz
+import seaborn as sns
 
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestorekey.json")
